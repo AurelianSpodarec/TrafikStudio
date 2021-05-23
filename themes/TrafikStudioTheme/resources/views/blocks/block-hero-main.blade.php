748da762061c47b44bf4b121ba8ca80cf2ce25e9
aurelianspodarec <aurelianxspodarec@gmail.com>
2021-05-23 22:00:39 +0100

    Created work item, needs improvement and mobile version

--- themes/TrafikStudioTheme/resources/views/blocks/block-hero-main.blade.php
+++ themes/TrafikStudioTheme/resources/views/blocks/block-hero-main.blade.php
@@ -1,5 +1,5 @@
 <x-section>
 
-Hero Main
+    <video loop="loop" preload="" muted="muted" playsinline="" autoplay="autoplay" class="slide__video js-slider__video"><source src="https://d2jd1xj0qdoqs5.cloudfront.net/craft/uploads/Home/B-Corp.mp4?mtime=20210517095645" type="video/mp4"></video>
 
 </x-section>
---
+++ themes/TrafikStudioTheme/resources/views/blocks/page-work.blade.php
@@ -0,0 +1,26 @@
+<x-section>
+<div class="mx-auto">
+<div class="py-10 px-12">
+
+
+    <div class="flex flex-wrap">
+    <article class="w-full work-item work-item--full">
+    <a href="">
+
+        <div class="work-item__inner">
+            <p class="work-item__client">Client Name</p>
+            <h2 class="work-item__heading">VFC... it's finger clickin' good!</h2>
+
+            <p class="work-item__cta">View Project</p>
+        </div>
+
+        <img class="work-item__img js-lazy is-active" src="https://d2jd1xj0qdoqs5.cloudfront.net/craft/uploads/Work/VFC/vfc_grid-wide.jpg?mtime=20210120145956" alt="Vfc Grid Mobile">
+
+    </a>
+    </article>
+    <div>
+
+
+</div>
+</div>
+</x-section>
